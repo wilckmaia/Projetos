7e45aad0fc60ba402dce24e3039ffd713cb7166f
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-06-29 15:54:18 +0000

    acertou ou errou

--- jogo de adivinhação/index.js
+++ jogo de adivinhação/index.js
@@ -1,3 +1,13 @@
 const SecretNum = Math.floor(Math.random() * 10) + 1;
 
-function verificar() {}
+function verificar() {
+  const input = document.getElementById("palpite");
+  const palpite = Number(input.value);
+  const mensagem = document.getElementById("mensagem");
+
+  if (palpite === numeroSecreto) {
+    mensagem.textContent = " Parabéns! Você acertou!";
+  } else {
+    mensagem.textContent = " Errou! Tente novamente.";
+  }
+}

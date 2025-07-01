8fd05d13e1b3550604f59183c3cc06fe96d58b9d
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-01 22:21:20 +0000

    adiciona as chances

--- jogo de adivinhação/index.js
+++ jogo de adivinhação/index.js
@@ -1,14 +1,21 @@
 const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let chances = 0;
 
 function verificar() {
   const input = document.getElementById("palpite");
   const palpite = Number(input.value);
   const mensagem = document.getElementById("mensagem");
 
+  chances++;
+
   if (palpite === numeroSecreto) {
     mensagem.textContent = " Parabéns! Você acertou!";
+  }
+  if (chances === 3) {
+    mensagem.textContent =
+      "Errou! Você usou 3 chances. O número secreto era: " + numeroSecreto;
   } else {
     mensagem.textContent =
-      " Errou! " + "O numero secreto era : " + numeroSecreto;
+      "Errou! Tente novamente. Chance " + chances + " de 3.";
   }
 }

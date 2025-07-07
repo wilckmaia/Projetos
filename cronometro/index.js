4b7a46e2bd2545bb7664571bc416b3abaddeaf02
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-07 01:22:03 +0000

    minutos e horas existem agora

--- cronometro/index.js
+++ cronometro/index.js
@@ -1,15 +1,23 @@
 let contador = 0;
-
+let minuto = 0;
+let hora = 0;
 const elementoContador = document.getElementById("display");
 
 function atualizarContador() {
   contador++;
-  elementoContador.textContent = contador;
+
+  if (contador === 60) {
+    minuto++;
+  } else if (minuto === 60) {
+    hora++;
+  }
+  elementoContador.textContent = hora + ":" + minuto + ":" + contador;
 }
 
 const intervalo = setInterval(atualizarContador, 1000);
 
 const botaoPausar = document.getElementById("pauseButton");
+
 botaoPausar.addEventListener("click", function () {
   clearInterval(intervalo);
 });
@@ -17,5 +25,6 @@ botaoPausar.addEventListener("click", function () {
 const botaoReset = document.getElementById("resetButton");
 
 botaoReset.addEventListener("click", function () {
-  elementoContador.textContent = 0;
+  elementoContador.textContent =
+    0 + "" + 0 + ":" + 0 + "" + 0 + ":" + 0 + "" + 0;
 });

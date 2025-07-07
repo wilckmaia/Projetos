cb034a89195a50b7eada9d040591e333bf24b859
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-07 19:42:42 +0000

    milésimos

--- cronometro/index.js
+++ cronometro/index.js
@@ -1,20 +1,26 @@
 let contador = 0;
+let segundo = 0;
 let minuto = 0;
-let hora = 0;
 const elementoContador = document.getElementById("display");
 
 function atualizarContador() {
   contador++;
 
   if (contador === 60) {
+    segundo++;
+    contador = 0;
+    contador++;
+  } else if (segundo === 60) {
     minuto++;
-  } else if (minuto === 60) {
-    hora++;
+    segundo = 0;
+    contador = 0;
+    segundo++;
+    contador++;
   }
-  elementoContador.textContent = hora + ":" + minuto + ":" + contador;
+  elementoContador.textContent = minuto + ":" + segundo + ":" + contador;
 }
 
-const intervalo = setInterval(atualizarContador, 1000);
+const intervalo = setInterval(atualizarContador, 100);
 
 const botaoPausar = document.getElementById("pauseButton");
 

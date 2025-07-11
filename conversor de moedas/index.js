6c92392be6dfb08b799dfc16dc6d13563cb5ce9a
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-11 18:35:43 +0000

    começando a funcionar

--- conversor de moedas/index.js
+++ conversor de moedas/index.js
@@ -1,13 +1,16 @@
-const input = document.getElementById("input");
 const select = document.getElementById("select");
-const display = document.getElementById("p");
-const select2 = document.getElementById("select2");
+const input = document.getElementById("input");
+const botaoPausar = document.getElementById("botaoPausar");
+const resultadoElemento = document.getElementById("resultado");
 
-let valorSelect = (select.value1 = 5);
-let valorSelect2 = (select.value1 = 1);
-let valorInput = Number(input) * valorSelect2;
-
-function change() {
-  const resultado = valorInput * valorSelect;
-  display.textContent = resultado;
-}
+botaoPausar.addEventListener("click", function () {
+  if (select.value === "1") {
+    const valorInput = Number(input.value);
+    const resultado = valorInput / 5;
+    resultadoElemento.textContent = "Resultado: " + resultado + " Dollars";
+  } else if (select.value === "2") {
+    const valorInput = Number(input.value);
+    const resultado2 = valorInput / 6;
+    resultadoElemento.textContent = "Resultado: " + resultado2 + " Euros";
+  }
+});

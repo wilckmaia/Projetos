64a412417d66bce4943bfdb53cd8fa190da74039
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-13 21:57:03 +0000

    melhorei na logica

--- conversor de moedas/index.js
+++ conversor de moedas/index.js
@@ -2,19 +2,22 @@ const select = document.getElementById("select");
 const input = document.getElementById("input");
 const botaoPausar = document.getElementById("botaoPausar");
 const resultadoElemento = document.getElementById("resultado");
+const selecionar = document.getElementById("selecionar");
 
 botaoPausar.addEventListener("click", function () {
-  if (select.value === "1") {
-    const valorInput = Number(input.value);
-    const resultado = valorInput / 5;
-    resultadoElemento.textContent = resultado + " Dollars";
-  } else if (select.value === "2") {
-    const valorInput = Number(input.value);
-    const resultado2 = valorInput / 6;
-    resultadoElemento.textContent = resultado2 + " Euros";
-  } else if (select.value === "3") {
-    const valorInput = Number(input.value);
-    const resultado3 = valorInput * 26;
-    resultadoElemento.textContent = resultado3 + " Ienes";
+  if (selecionar.value === "1") {
+    if (select.value === "1") {
+      const valorInput = Number(input.value);
+      const resultado = valorInput / 5;
+      resultadoElemento.textContent = resultado + " Dollars";
+    } else if (select.value === "2") {
+      const valorInput = Number(input.value);
+      const resultado2 = valorInput / 6;
+      resultadoElemento.textContent = resultado2 + " Euros";
+    } else if (select.value === "3") {
+      const valorInput = Number(input.value);
+      const resultado3 = valorInput * 26;
+      resultadoElemento.textContent = resultado3 + " Ienes";
+    }
   }
 });

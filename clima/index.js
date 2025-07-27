c20a87c05d8cd5fec7c1fc6a06cc1eb32fbc0b3c
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-07-27 00:10:02 +0000

    backend pronto

--- clima/index.js
+++ clima/index.js
@@ -1,44 +1,83 @@
 const estados = document.getElementById("estados");
-const cidadeSp = document.getElementById("cidades-sp");
-const cidadeMg = document.getElementById("cidades-mg");
-const butao = document.getElementById("button");
 const display = document.getElementById("display");
 const clima = document.getElementById("clima");
+const butao2 = document.getElementById("button2");
 
 const sp = "1";
-const mg = "2";
-const campinas = "1";
-const ubatuda = "2";
-const BeloHorizonte = "1";
-const uberlandia = "2";
+const rj = "2";
+const br = "3";
 
 async function saoPaulo() {
   const response = await fetch(
     "https://api.open-meteo.com/v1/forecast?latitude=-23.55&longitude=-46.63&current_weather=true"
   );
   const result = await response.json();
-  return result["latitude"];
+  const temperatura = result["current_weather"]["temperature"];
+  const vento = result["current_weather"]["windspeed"];
+  const exiba =
+    "Temperatura :  " +
+    temperatura +
+    " ° " +
+    " Velocidade Vento :  " +
+    vento +
+    " km/h  ";
+  return exiba;
 }
-async function mostrarLatitude() {
-  const latitudesp = await saoPaulo();
-  console.log(latitudesp); // aqui vai mostrar o valor real
+async function mostrarDadosSP() {
+  const dados = await saoPaulo();
+  console.log(dados);
+  display.textContent = dados;
 }
 
-mostrarLatitude();
+async function rioDeJaneiro() {
+  const response = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=-22.91&longitude=-43.17&current_weather=true"
+  );
+  const result = await response.json();
+  const temperatura = result["current_weather"]["temperature"];
+  const vento = result["current_weather"]["windspeed"];
+  const exiba =
+    "Temperatura :  " +
+    temperatura +
+    " ° " +
+    " Velocidade Vento :  " +
+    vento +
+    " km/h ";
+  return exiba;
+}
+async function mostrarDadosRj() {
+  const dados = await rioDeJaneiro();
+  console.log(dados);
+  display.textContent = dados;
+}
 
-butao.addEventListener("click", () => {
-  console.log(mostrarLatitude);
+async function brasilia() {
+  const response = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=-15.79&longitude=-47.88&current_weather=true"
+  );
+  const result = await response.json();
+  const temperatura = result["current_weather"]["temperature"];
+  const vento = result["current_weather"]["windspeed"];
+  const exiba =
+    "Temperatura :  " +
+    temperatura +
+    " ° " +
+    " Velocidade Vento :  " +
+    vento +
+    " km/h ";
+  return exiba;
+}
+async function mostrarDadosBr() {
+  const dados = await brasilia();
+  display.textContent = dados;
+}
 
+butao2.addEventListener("click", async function () {
   if (estados.value === sp) {
-    console.log("tudo certinho, chefe!");
-    cidadeMg.style.display = "none";
-    cidadeSp.style.display = "inline-block";
-    display.style.display = "inline-block";
-    clima.style.display = "inline-block";
-  } else if (estados.value === mg) {
-    cidadeSp.style.display = "none";
-    cidadeMg.style.display = "inline-block";
-    display.style.display = "inline-block";
-    clima.style.display = "inline-block";
+    const climasp = await mostrarDadosSP();
+  } else if (estados.value === rj) {
+    const climarj = await mostrarDadosRj();
+  } else if (estados.value === br) {
+    const climabrvalue = mostrarDadosBr();
   }
 });

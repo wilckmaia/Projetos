52e8ee431d5de54d184a316c698534aabd1364ef
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-08-01 00:03:21 +0000

    back pronto

--- sortear carta/index.js
+++ sortear carta/index.js
@@ -3,24 +3,22 @@ const display = document.getElementById("display");
 
 async function aleatorio() {
   const response = await fetch(
-    "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
+    "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
   );
   const result = await response.json();
-  const exiba = result["cards"];
-  return exiba;
+  const carta = result.cards[0];
+  return carta;
 }
 
 async function dados() {
-  const dados = await aleatorio();
-  console.log(dados);
-  dados.forEach((carta) => {
-    const p = document.createElement("p");
-    p.textContent = `${carta.value} of ${carta.suit}`;
-    display.appendChild(p);
-  });
+  display.textContent = "";
+
+  const carta = await aleatorio();
+  console.log(carta);
+
+  const p = document.createElement("p");
+  p.textContent = `${carta.value} of ${carta.suit}`;
+  display.appendChild(p);
 }
 
-botao.addEventListener("click", async function () {
-  console.log(dados);
-  const mostrar = await dados();
-});
+botao.addEventListener("click", dados);

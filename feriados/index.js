1fec81b9c75dd60038c7bc9a41bd096f1e471347
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-08-04 23:49:19 +0000

    progredindo

--- feriados/index.js
+++ feriados/index.js
@@ -1,15 +1,24 @@
 const botao = document.getElementById("botao");
+const display = document.getElementById("display");
 
-async function dados() {
+async function aleatorio() {
   const response = await fetch(
     " https://brasilapi.com.br/api/feriados/v1/2025"
   );
   const result = await response.json();
-  const dados = result["date"];
-  console.log(dados);
-  return dados;
+  const carta = result["date"];
+  return carta;
 }
 
-botao.addEventListener("click", async function () {
-  const displayIt = await dados();
-});
+async function dados() {
+  display.textContent = "";
+
+  const carta = await aleatorio();
+  console.log(carta);
+
+  const p = document.createElement("p");
+  p.textContent = `${carta.value} of ${carta.suit}`;
+  display.appendChild(p);
+}
+
+botao.addEventListener("click", dados);
